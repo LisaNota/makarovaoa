f4915ef50be281dceaf5271746d58dcee4f2b8ab
LisaNota <113865762+LisaNota@users.noreply.github.com>
2023-04-13 19:03:48 +0300

    Update convex_bucket.py

--- practicum_5/homework/convex_bucket.py
+++ practicum_5/homework/convex_bucket.py
@@ -5,16 +5,34 @@ from numpy.typing import NDArray
 
 from src.plotting import plot_points
 
+def compare(x: list, y: list) -> int:
+    # sort the vertices first by x-coordinate, then by y-coordinate
+    if (x[0] < y[0]) or (x[0] == y[0] and x[1] < y[1]):
+        return -1
+    elif (x[0] == y[0] and x[1] == y[1]):
+        return 0
+    return 1
+
+def rotate(x: list, y: list, z: list) -> bool:
+    # this function determines which side of the vector XY is the point Z
+    if np.sign((z[0]-y[0])*(y[1]-x[1]) - (z[1]-y[1])*(y[0]-x[0])) >= 0:
+        return True
+    return False
 
 def convex_bucket(points: NDArray) -> NDArray:
     """Complexity: O(n log n)"""
     clockwise_sorted_ch = []
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    points = sorted(points, key=cmp_to_key(compare))
+    clockwise_sorted_ch.append(points[0])
+    clockwise_sorted_ch.append(points[1])
 
-    return np.array(clockwise_sorted_ch)
+    for point in points[2::]:
+        while (len(clockwise_sorted_ch) > 1) and (rotate(clockwise_sorted_ch[-2], clockwise_sorted_ch[-1], point)):
+            clockwise_sorted_ch.pop()
+        clockwise_sorted_ch.append(point)
+        
+    return np.array(clockwise_sorted_ch + clockwise_sorted_ch[-2::-1])
 
 
 if __name__ == "__main__":
